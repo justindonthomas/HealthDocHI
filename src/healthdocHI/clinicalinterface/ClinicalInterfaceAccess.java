b8f2c5261e4ef6d1d9b9c5c9aec27a43d5cce3bf
justindonthomas <justin.don.thomas@gmail.com>
2017-04-20 09:20:30 -0600

    Interface to the Clinical Interface for sending data to/from the Clinical Interface bound for the server.

---
+++ src/healthdocHI/clinicalinterface/ClinicalInterfaceAccess.java
@@ -0,0 +1,16 @@
+package healthdocHI.clinicalinterface;
+
+import healthdocHI.clinicalinterface.humanInterfaceMessages.DataAlterations;
+import healthdocHI.clinicalinterface.humanInterfaceMessages.DataRequest;
+import healthdocHI.clinicalinterface.humanInterfaceMessages.UserAuthentication;
+
+/**
+ * Interface that the human interface will use to access the client side of the
+ * clinical interface.
+ * Created by jdt on 4/20/17.
+ */
+public interface ClinicalInterfaceAccess {
+  public void sendLoginRequest(UserAuthentication login);
+  public void sendDataRequest(DataRequest request);
+  public void sendDataAlteration(DataAlterations changes);
+}
