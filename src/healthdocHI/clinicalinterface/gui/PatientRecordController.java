267934545a9e46a3435164282727e1bdc56c02df
Nick <Nick@Maxam-PC>
2017-04-24 10:50:36 -0600

    Patient record entries for visits now populate with dates instead of numbers.
    Also added updated jar

--- src/healthdocHI/clinicalinterface/gui/PatientRecordController.java
+++ src/healthdocHI/clinicalinterface/gui/PatientRecordController.java
@@ -1,6 +1,9 @@
 package healthdocHI.clinicalinterface.gui;
 
 import java.net.URL;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import healthdocHI.clinicalinterface.ProxyServer;
@@ -20,6 +23,8 @@ public class PatientRecordController implements Initializable
 
   private ProxyServer server;
   private PatientRecord currentRecord;
+  //Maps dates to visit numbers to pull correct visit log
+  private HashMap<String, Integer> visitMap;
   
   @FXML
   private TextField name;
@@ -46,7 +51,7 @@ public class PatientRecordController implements Initializable
   @Override
   public void initialize(URL arg0, ResourceBundle arg1)
   {
-
+    visitMap = new HashMap<>();
     server = new ProxyServer();
     DataRequest request = new DataRequest(DataRequest.RequestType.PATIENT_LIST, "0001");
     PatientList pList = (PatientList)server.sendDataRequest(request);
@@ -68,7 +73,7 @@ public class PatientRecordController implements Initializable
       staff.clear();
       return;
     }
-    int visitNum = Integer.valueOf(patientEntryComboBox.getValue());
+    int visitNum = visitMap.get(patientEntryComboBox.getValue());
     if (currentRecord != null)
     {
       VisitRecord record = currentRecord.getVisitRecord(visitNum);
@@ -85,6 +90,7 @@ public class PatientRecordController implements Initializable
   private void handleIdSelect()
   {
 
+    visitMap.clear();
     String id = patientIdComboBox.getValue();
     DataRequest request = new DataRequest(DataRequest.RequestType.PATIENT_RECORD, id);
     PatientRecord record = (PatientRecord)server.sendDataRequest(request);
@@ -96,9 +102,16 @@ public class PatientRecordController implements Initializable
     
     patientEntryComboBox.getItems().clear();
     int numVisits = record.getVisitNumber();
+    VisitRecord visit;
+    String date;
+    GregorianCalendar calendar;
     for (int i = 0; i < numVisits; i++)
     {
-      patientEntryComboBox.getItems().add(String.valueOf(i));
+      visit = record.getVisitRecord(i);
+      calendar = visit.getDate();
+      date = calendar.get(Calendar.MONTH) + "/" + calendar.get(Calendar.DAY_OF_MONTH) + "/" + calendar.get(Calendar.YEAR);
+      patientEntryComboBox.getItems().add(date);
+      visitMap.put(date, i);
     }
     
   }
